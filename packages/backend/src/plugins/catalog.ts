5feedda72337f6693842915fa8ad8eae0b203cfa
Jess Mann <me@jessmann.io>
2024-03-18 22:54:05 +0100

    remove deprecated usage of GH discovery processor

--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -1,32 +1,20 @@
 import { CatalogBuilder } from '@backstage/plugin-catalog-backend';
+import { GithubEntityProvider } from '@backstage/plugin-catalog-backend-module-github';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
-import {
-  GithubDiscoveryProcessor,
-  GithubOrgReaderProcessor,
-} from '@backstage/plugin-catalog-backend-module-github';
-import {
-  ScmIntegrations,
-  DefaultGithubCredentialsProvider,
-} from '@backstage/integration';
+
 
 export default async function createPlugin(
   env: PluginEnvironment,
 ): Promise<Router> {
   const builder = await CatalogBuilder.create(env);
-  const integrations = ScmIntegrations.fromConfig(env.config);
-  const githubCredentialsProvider =
-    DefaultGithubCredentialsProvider.fromIntegrations(integrations);
-  builder.addProcessor(
-    GithubDiscoveryProcessor.fromConfig(env.config, {
+  builder.addEntityProvider(
+    GithubEntityProvider.fromConfig(env.config, {
       logger: env.logger,
-      githubCredentialsProvider,
-    }),
-    GithubOrgReaderProcessor.fromConfig(env.config, {
-      logger: env.logger,
-      githubCredentialsProvider,
+      scheduler: env.scheduler,
     }),
   );
+
   const { processingEngine, router } = await builder.build();
   await processingEngine.start();
   return router;
