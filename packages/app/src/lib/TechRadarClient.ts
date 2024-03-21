e7db5ecb7eeb5448b740e87609ea3810671b7ae6
Jess Mann <me@jessmann.io>
2024-03-21 20:42:57 +0100

    just gh won't work due to CORS

--- packages/app/src/lib/TechRadarClient.ts
+++ packages/app/src/lib/TechRadarClient.ts
@@ -4,12 +4,14 @@ import {
   TechRadarApi,
   TechRadarLoaderResponse,
 } from '@backstage/plugin-tech-radar';
+import { radar } from './radar';
   
   export class TechRadarClient implements TechRadarApi {
     async load(): Promise<TechRadarLoaderResponse> {
       // if needed id prop can be used to fetch the correct data
   
-      const data = await fetch('https://github.com/all-boundaries/tech-radar/blob/main/radar.json').then(res => res.json());
+      // const data = await fetch('https://github.com/all-boundaries/tech-radar/blob/main/radar.json').then(res => res.json());
+      const data = radar
   
       // For example, this converts the timeline dates into date objects
       return {
