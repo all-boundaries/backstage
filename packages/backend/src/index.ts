48f46c50494dd9122ddccd969c7348299bb98d30
Jess Mann <me@jessmann.io>
2025-03-23 13:20:57 +0100

    auto discover catalog entities in github org

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -22,6 +22,7 @@ backend.add(import('@backstage/plugin-auth-backend-module-github-provider'));
 
 // catalog plugin
 backend.add(import('@backstage/plugin-catalog-backend'));
+backend.add(import('@backstage/plugin-catalog-backend-module-github'));
 backend.add(
   import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'),
 );
